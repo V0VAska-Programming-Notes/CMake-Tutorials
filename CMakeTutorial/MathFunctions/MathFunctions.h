dd88fbf97ae0771016396f44a0609c211839b3a5
V0VAska <55778679+V0VAska@users.noreply.github.com>
2022-11-05 00:51:37 +1000

    Fix static linking (Windows)

--- CMakeTutorial/MathFunctions/MathFunctions.h
+++ CMakeTutorial/MathFunctions/MathFunctions.h
@@ -1,20 +1,8 @@
 #pragma once
 
-// TODO:При компиляции под форточками невозможно статическое связывание.
-// Варианты лечения на выбор:
-// 1. CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS
-// 2. Как намекается в туторе, использовать модуль GenerateExportHeader, что предпочтительнее
-#if defined(_WIN32)
-#   if defined(MATHLIBRARY_EXPORT)
-#       define MATHFUNCTIONS_API __declspec(dllexport)
-#   else
-#       define MATHFUNCTIONS_API __declspec(dllimport)
-#   endif
-#else
-#   define MATHFUNCTIONS_API
-#endif
+#include "mathfunctions_export.h"
 
 namespace mathfunctions
 {
-    MATHFUNCTIONS_API double sqrt(double);
+    MATHFUNCTIONS_EXPORT double sqrt(double);
 }
